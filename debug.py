7f5f578c15fb7b2c01ab0ca09f0e2138e54446bf
madalinefitzpatrick <madaline.fitzpatrick@gmail.com>
2023-03-20 16:43:31 -0400

    deliverables

--- debug.py
+++ debug.py
@@ -1,11 +1,21 @@
 from lib.animal import *
 from lib.zoo import *
 
+
 # code here
 
 # e.g.  
-#   z1 = Zoo( 'Micke Grove Zoo', 'Lodi, CA' )
-#   a1 = Animal( 'Lion', 75, 'Luke', z1 )
+z1 = Zoo( 'Micke Grove Zoo', 'Lodi, CA' )
+z2 = Zoo('the best', 'Detroit, MI')
+z3 = Zoo('san fran', 'san fran, CA')
+z4 = Zoo('cool zoo', 'san fran, CA')
+
+
+a1 = Animal( 'Lion', 75, 'Luke', z1)
+a2 = Animal('Monkey', 50, 'Phil', z1)
+a3 = Animal( 'Giraffe', 100, 'Gina', z2)
+a4 = Animal('Parrot', 20, 'Jim', z3)
+a5 = Animal('Monkey', 50, 'Johnny', z1)
 
 
 
--- lib/zoo.py
+++ lib/zoo.py
@@ -1,4 +1,36 @@
 from lib.animal import Animal
 
 class Zoo:
-    pass
+    all=[]
+
+    def __init__(self, name, location):
+        self.name=name
+        self.location=location
+        Zoo.all.append(self)
+    
+    @property
+    def animals(self):
+        return[a for a in Animal.all if a.zoo_instance == self]
+
+    @property
+    def animal_species(self):
+        return list({a.species for a in Animal.all if a.zoo_instance == self})
+
+    
+    def find_by_species(self, species_finder):
+        return [a.nickname for a in self.animals if a.species == species_finder]
+
+    
+    @property
+    def animal_nicknames(self):
+        return [a.nickname for a in self.animals]
+
+    @classmethod
+    def find_by_location(cls, given_location):
+        return [z.name for z in cls.all if z.location == given_location]
+    
+
+    
+
+
+
